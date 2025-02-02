848bbdd814c02822d6c9f98348f3825469d4c0f9
binaryBard73 <c.corsetti.01@gmail.com>
2025-02-02 19:11:26 +0100

    build: add base

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   site: "https://cors.industries",
+  base: "/",  
   output: "static",
   integrations: [
     expressiveCode(),
